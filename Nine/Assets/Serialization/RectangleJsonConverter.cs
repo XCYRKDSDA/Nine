408eea19c20c7c39e8c5f13ee416a0157c82caed
XCYRKDSDA <personalXCY@outlook.com>
2023-08-06 16:31:18 +0800

    基本数据类型序列化

---
+++ Nine/Assets/Serialization/RectangleJsonConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Xna.Framework;
+
+namespace Nine.Assets.Serialization;
+
+public class RectangleJsonConverter : JsonConverter<Rectangle>
+{
+    public override Rectangle Read(ref Utf8JsonReader reader, Type typeToConvert,
+                                   JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+            throw new JsonException();
+
+        var rectangle = new Rectangle();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                return rectangle;
+
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var property = reader.GetString();
+
+                reader.Read();
+                var value = reader.GetInt32();
+
+                if (property == "x" || property == "X")
+                    rectangle.X = value;
+                else if (property == "y" || property == "Y")
+                    rectangle.Y = value;
+                else if (property == "w" || property == "W")
+                    rectangle.Width = value;
+                else if (property == "h" || property == "H")
+                    rectangle.Height = value;
+                else
+                    throw new JsonException();
+            }
+        }
+
+        throw new JsonException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, Rectangle value,
+                               JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Nine/Assets/Serialization/Vector3JsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Xna.Framework;
+
+namespace Nine.Assets.Serialization;
+
+public class Vector3JsonConverter : JsonConverter<Vector3>
+{
+    public override Vector3 Read(ref Utf8JsonReader reader, Type typeToConvert,
+                                 JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartArray)
+            throw new JsonException();
+
+        // 将数组前两个值记录为x和y
+        reader.Read();
+        var x = reader.GetSingle();
+        reader.Read();
+        var y = reader.GetSingle();
+        reader.Read();
+        var z = reader.GetSingle();
+
+        // 丢弃多余的数组成员
+        do
+            reader.Read();
+        while (reader.TokenType != JsonTokenType.EndArray);
+
+        return new(x, y, z);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Vector3 value,
+                               JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}

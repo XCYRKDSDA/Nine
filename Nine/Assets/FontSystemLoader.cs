9e2e84800e6c821debc0740a7d2a3ab65676ac53
XCYRKDSDA <personalXCY@outlook.com>
2023-08-07 14:00:27 +0800

    基于FontStashSharp的字体加载

---
+++ Nine/Assets/FontSystemLoader.cs
@@ -0,0 +1,15 @@
+﻿using FontStashSharp;
+
+namespace Nine.Assets;
+
+public class FontSystemLoader : IAssetLoader<FontSystem>
+{
+    public FontSystem Load(AssetsContext context, string asset)
+    {
+        var fontSystem = new FontSystem();
+        foreach (var font in asset.Split(','))
+            fontSystem.AddFont(context.Open(font));
+
+        return fontSystem;
+    }
+}

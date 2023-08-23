f43b7ce48c99e021e1807c1cf80a2de95a76de7d
XCYRKDSDA <personalXCY@outlook.com>
2023-08-23 19:17:19 +0800

    用回.Net6，并实现了三种数据类型的Hermite Cubic曲线

---
+++ Nine/Animations/CubicCurve3.cs
@@ -0,0 +1,88 @@
+﻿using ValueT = Microsoft.Xna.Framework.Vector3;
+
+namespace Nine.Animations;
+
+public class CubicCurve3 : ICurve<ValueT>
+{
+    private readonly CubicCurveKeyCollection<ValueT> _keys = new();
+
+    public CubicCurveKeyCollection<ValueT> Keys => _keys;
+
+    private (int, int) FindKeysIndices(float position)
+    {
+        if (position < _keys[0].Position)
+            return (-1, 0);
+        if (position > _keys[^1].Position)
+            return (_keys.Count - 1, -1);
+
+        int l = 0, r = _keys.Count - 1;
+        while (true)
+        {
+            if (r == l + 1)
+                return (l, r);
+
+            var m = (l + r) / 2;
+
+            if (_keys[m].Position < position)
+                l = m;
+            else if (_keys[m].Position > position)
+                r = m;
+            else
+                return (m, m);
+        }
+    }
+
+    private ValueT GetGradient(int idx)
+    {
+        if (idx <= 0 || idx >= _keys.Count - 1)
+            return ValueT.Zero;
+
+        var key = _keys[idx];
+
+        if (key.Gradient.HasValue)
+            return key.Gradient.Value;
+
+        var prevKey = _keys[idx - 1];
+        var nextKey = _keys[idx + 1];
+
+        return (nextKey.Value - prevKey.Value) / (nextKey.Position - prevKey.Position);
+    }
+
+    public ValueT Evaluate(float position)
+    {
+        // 首先找到位置两侧的关键帧
+        var (leftKeyIdx, rightKeyIdx) = FindKeysIndices(position);
+
+        // 处理特殊情况
+        if (leftKeyIdx < 0)
+            return Keys[0].Value;
+        else if (rightKeyIdx < 0)
+            return Keys[^1].Value;
+        else if (leftKeyIdx == rightKeyIdx)
+            return Keys[leftKeyIdx].Value;
+
+        var leftKey = Keys[leftKeyIdx];
+        var rightKey = Keys[rightKeyIdx];
+
+        // 确定两侧关键帧上的值和斜率
+        var p0 = leftKey.Value;
+        var p1 = rightKey.Value;
+        var m0 = GetGradient(leftKeyIdx);
+        var m1 = GetGradient(rightKeyIdx);
+
+        // 标准化
+        var u = rightKey.Position - leftKey.Position;
+        m0 /= u;
+        m1 /= u;
+        var t = (position - leftKey.Position) / u;
+
+        // 计算结果
+        var t2 = t * t;
+        var t3 = t2 * t;
+        var h00 = 2 * t3 - 3 * t2 + 1;
+        var h10 = t3 - 2 * t2 + t;
+        var h01 = -2 * t3 + 3 * t2;
+        var h11 = t3 - t2;
+        return h00 * p0 + h10 * m0 + h01 * p1 + h11 * m1;
+    }
+}
---
+++ Nine/Animations/CubicCurveKey.cs
@@ -0,0 +1,29 @@
+﻿namespace Nine.Animations;
+
+public struct CubicCurveKey<T> : IEquatable<CubicCurveKey<T>>
+    where T : struct, IEquatable<T>
+{
+    public float Position;
+
+    public T Value;
+
+    public T? Gradient;
+
+    public CubicCurveKey(float position, T value, T? gradient = null)
+    {
+        Position = position;
+        Value = value;
+        Gradient = gradient;
+    }
+
+    public readonly bool Equals(CubicCurveKey<T> other) => Position.Equals(other.Position) && Value.Equals(other.Value) && Gradient!.Equals(other.Gradient);
+
+    public override readonly bool Equals(object? obj) => obj is CubicCurveKey<T> other && Equals(other);
+
+    public static bool operator ==(CubicCurveKey<T> left, CubicCurveKey<T> right) => left.Equals(right);
+
+    public static bool operator !=(CubicCurveKey<T> left, CubicCurveKey<T> right) => !(left == right);
+
+    public override readonly int GetHashCode() => Position.GetHashCode() ^ Value.GetHashCode() ^ Gradient!.GetHashCode();
+
+}

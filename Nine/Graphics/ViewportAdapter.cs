00f4e87edb22b437f70136fa27b68f65916976a5
XCYRKDSDA <personalXCY@outlook.com>
2025-01-13 09:35:56 +0800

    refactor: remove size types

--- Nine/Graphics/ViewportAdapter.cs
+++ Nine/Graphics/ViewportAdapter.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-using Nine.Drawing;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Nine.Graphics;
 
 public static class ViewportAdapter
 {
-    public static Viewport Adapt(in SizeF expectSize, in Viewport viewport,
+    public static Viewport Adapt(in Point expectSize, in Viewport viewport,
                                  float maxScale = float.PositiveInfinity, float minScale = 0)
     {
-        var scaleX = viewport.Width / expectSize.Width;
-        var scaleY = viewport.Height / expectSize.Height;
+        var scaleX = viewport.Width / expectSize.X;
+        var scaleY = viewport.Height / expectSize.Y;
 
         float scale;
         if (scaleX > scaleY)
             scale = MathF.Max(minScale, MathF.Min(maxScale, scaleY));
         else
             scale = MathF.Max(minScale, MathF.Min(maxScale, scaleX));
-        var actualWidth = (int)(expectSize.Width * scale);
-        var actualHeight = (int)(expectSize.Height * scale);
+        var actualWidth = (int)(expectSize.X * scale);
+        var actualHeight = (int)(expectSize.Y * scale);
 
         return new((viewport.Width - actualWidth) / 2, (viewport.Height - actualHeight) / 2, actualWidth, actualHeight);
     }

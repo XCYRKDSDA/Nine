4200e3ccb850033a43d3035644a1bf46e24bd7bc
XCYRKDSDA <personalXCY@outlook.com>
2025-11-03 23:58:27 +0800

    refactor(assets): use XNA's default color processors

--- Nine/Assets/Texture2DLoader.cs
+++ Nine/Assets/Texture2DLoader.cs
@@ -1,4 +1,3 @@
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Zio;
 
@@ -13,37 +12,10 @@ public class Texture2DLoader : IAssetLoader<Texture2D>
         GraphicsDevice = graphicsDevice;
     }
 
-    private static byte ApplyAlpha(byte color, byte alpha)
-    {
-        var fc = color / 255.0f;
-        var fa = alpha / 255.0f;
-        var fr = (int)(255.0f * fc * fa);
-        if (fr < 0)
-            fr = 0;
-        else if (fr > 255)
-            fr = 255;
-        return (byte)fr;
-    }
-
     public Texture2D Load(IFileSystem fs, IAssetsManager assets, in UPath path)
     {
         using var fileStream = fs.OpenFile(path, FileMode.Open, FileAccess.Read);
 
-        var rawTexture = Texture2D.FromStream(GraphicsDevice, fileStream);
-
-        var data = new Color[rawTexture.Width * rawTexture.Height];
-        rawTexture.GetData(data);
-        for (int i = 0; i < data.Length; ++i)
-        {
-            byte a = data[i].A;
-
-            data[i].R = ApplyAlpha(data[i].R, a);
-            data[i].G = ApplyAlpha(data[i].G, a);
-            data[i].B = ApplyAlpha(data[i].B, a);
-        }
-
-        var result = new Texture2D(GraphicsDevice, rawTexture.Width, rawTexture.Height);
-        result.SetData(data);
-        return result;
+        return Texture2D.FromStream(GraphicsDevice, fileStream, DefaultColorProcessors.PremultiplyAlpha);
     }
 }
